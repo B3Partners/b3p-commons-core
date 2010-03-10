e2eef8d48fd452a2f5ef14f2a70f5fca297d1cd3
Meine Toonen <toonen.meine@gmail.com>
2010-03-10 15:24:20 +0000

    Stripes JpaTransactionInterceptor en Transactional toegevoegd

---
+++ src/nl/b3p/commons/stripes/Transactional.java
@@ -0,0 +1,14 @@
+package nl.b3p.commons.stripes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Transactional {
+    public static final String DEFAULT_PERSISTENCE_UNIT = "[default]";
+
+    String persistenceUnit() default DEFAULT_PERSISTENCE_UNIT;
+}
